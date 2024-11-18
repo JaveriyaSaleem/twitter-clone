81035e9102df005e8182282f411fa67b7c8aa36a
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2024-11-18 19:35:26 +0500

    added

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-import { getAuth, createUserWithEmailAndPassword,GoogleAuthProvider,signInWithPopup } from "./firebase.js";
+import { getAuth, createUserWithEmailAndPassword,GoogleAuthProvider,signInWithPopup,
+    // firestore 
+    db,collection, addDoc
+ } from "./firebase.js";
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
 let registerBtn = document.getElementById('register')
@@ -7,28 +10,33 @@ let getPassword = document.getElementById('password')
 let getName = document.getElementById('fullName')
 let googleBtn = document.getElementById('google')
 // click on register btn 
-registerBtn && registerBtn.addEventListener('click', () => {
+registerBtn && registerBtn.addEventListener('click',() => {
+const email = getEmail.value.trim();
+const password = getPassword.value.trim();
+const fullName = getName.value.trim();
     createUserWithEmailAndPassword(auth, getEmail.value, getPassword.value)
         .then((userCredential) => {
             // Signed up 
             const user = userCredential.user;
             console.log(user)
-            // ...
-            const Toast = Swal.mixin({
-                toast: true,
-                position: "top-end",
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.onmouseenter = Swal.stopTimer;
-                    toast.onmouseleave = Swal.resumeTimer;
-                }
-            });
-            Toast.fire({
-                icon: "success",
-                title: "Signed in successfully"
-            });
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.onmouseenter = Swal.stopTimer;
+                        toast.onmouseleave = Swal.resumeTimer;
+                    }
+                });
+                Toast.fire({
+                    icon: "success",
+                    title: "Signed in successfully"
+                });
+
+
+
             setTimeout(()=>{
             location.href = "./Dashboard/dashboard.html"
             },3000)
@@ -92,3 +100,4 @@ googleBtn.addEventListener('click',(()=>{
     const credential = GoogleAuthProvider.credentialFromError(error);
   });
 }))
+
--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getFirestore,collection, addDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged,sendPasswordResetEmail,GoogleAuthProvider,signInWithPopup,updateProfile } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 const firebaseConfig = {
   apiKey: "AIzaSyAZi9QOzSTGsNmNjsiGgEaXwNcDaUd3WE4",
@@ -8,7 +9,7 @@ const firebaseConfig = {
   messagingSenderId: "6481384555",
   appId: "1:6481384555:web:c2fa04d60c3e535e114d2e"
 };
-
-
 const app = initializeApp(firebaseConfig);
-export { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged,sendPasswordResetEmail,GoogleAuthProvider,signInWithPopup,updateProfile }
+const db = getFirestore(app);
+export { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged,sendPasswordResetEmail,GoogleAuthProvider,signInWithPopup,updateProfile,db,collection, addDoc
+ }
