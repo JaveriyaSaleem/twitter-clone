3e47ec8dfecc2df1094162c10c4657fdfdce018e
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2024-11-18 21:39:57 +0500

    added

--- PFS/pfs.js
+++ PFS/pfs.js
@@ -1,45 +1,51 @@
-import { getAuth, onAuthStateChanged,updateProfile } from "../firebase.js";
+import { getAuth, onAuthStateChanged,updateProfile,doc, updateDoc,db } from "../firebase.js";
 let emailGet = document.getElementById('getEmail')
 let nameUpdate = document.getElementById("Name")
 let nameUpdate2 = document.getElementById("Name2")
 let professionUpdate = document.getElementById("profession")
 let professionUpdate2 = document.getElementById("profession2")
 let phone = document.getElementById('phoneNumber')
-let name;
-let profession;
 const auth = getAuth();
 let editBtn = document.getElementById('editBtn')
+// edit button 
 editBtn.addEventListener('click',(()=>{
-name = prompt("Name",nameUpdate.innerHTML)
-profession = prompt("Profession",professionUpdate.innerHTML)
-let phoneNum = prompt("Phone Number",phone.innerHTML)
-  updateProfile(auth.currentUser, {
-    displayName: name, photoURL: "https://example.com/jane-q-user/profile.jpg",phoneNumber: phoneNum,
-  }).then(() => {
-    alert("profile updated!")
-    nameUpdate.innerHTML = name
-    nameUpdate2.innerHTML = name
-    professionUpdate.innerHTML = profession
-    professionUpdate2.innerHTML = profession
-    phone.innerHTML = phoneNum
+let name = prompt("Name",nameUpdate.innerHTML)
+let profession = prompt("Profession",professionUpdate.innerHTML)
+let phoneNumber = prompt("Add your Number")
+let address = prompt('Address')
+// if user is sign in 
+onAuthStateChanged(auth, async(user) =>{
+  if (user) {
+   console.log(user)
+    const uid = user.uid;
+    const myData = doc(db, "userData", uid);
+    await updateDoc(myData, {
+    FullName:name,
+    Profession: profession,
+    PhoneNumber: phoneNumber,
+    Address:address
+});
+try{
+  await updateProfile(auth.currentUser, {
+    displayName: name
+  })
+  alert("Profile updated!");
+  nameUpdate.innerHTML = name
+  nameUpdate2.innerHTML = name
+  professionUpdate.innerHTML = profession
+  professionUpdate2.innerHTML = profession
+  phone.innerHTML = phoneNum;
 
+  }catch(e){
 
-  }).catch((error) => {
-    // An error occurred
-    // ...
-  });
+  } 
+  
+}else{
+  console.log("user is signout")
+}});
+
 }))
 
 
 
 
-// if user is sign in 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-   console.log(user)
-    const uid = user.uid;
-    emailGet.innerHTML = user.email
-  } else {
-    console.log("user is signed out")
-  }
-});
